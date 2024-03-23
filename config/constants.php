1e462fad28c60deead6f64f76b978b7899bc7bcd
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2024-03-23 12:30:32 +0530

    first commit

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+define("ROOT_URL", "http://localhost/blog_app/");
+define('DB_HOST', 'localhost');
+define('DB_USER', 'pratik');
+define('DB_PASS', 'pratik');
+define('DB_NAME', 'blog');
+
